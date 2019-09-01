3a50a9de1633c989a7bcbb0535c463eba0c472fc
awoodcroft95 <awoodcroft95@gmail.com>
2019-09-01 01:15:03 +0100

    Added multiplle commands per message/audio file. Better error handling, new commands. Updated README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AntstarDiscordBot
 Discord Server Bot that can play music and send messages based on commands
+
+
+To Make this function nadd a auth.json file in the root directory of the project and fill it with and object with the following format:
+
+{
+    "token": "yourapptoken"
+}
--- bot.js
+++ bot.js
@@ -23,21 +23,37 @@ bot.on('ready', () => {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', message => {
+    const commandPostpend = ", ";
+    message.content = message.content.toLowerCase();
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (isReady && message.content.substring(0, 1) == '!') {
         isReady = false;
         var args = message.content.substring(1).split(' ');
         var cmd = args[0];
-       
-        args = args.splice(1);
-        messages.forEach(function(commandPair){
-            commandPair.command.forEach(function(cmdText){
-                if (cmdText == cmd){
-                    message.channel.send(commandPair.message);
-                }
+
+        var commandListString = "The Text Commands are:\n";
+        var commandPrepend = "!";
+        if (cmd === "commands"){
+            messages.forEach(function(commandPair){
+                commandPair.command.forEach(function(cmdText){
+                    commandListString += commandPrepend + cmdText + commandPostpend;
+                });
+                commandListString = commandListString.substring(0, commandListString.length - 2);
+                commandListString += " **|** "
             });
-        })
+            commandListString = commandListString.substring(0, commandListString.length - 7);
+            message.channel.send(commandListString);
+        } else {
+            args = args.splice(1);
+            messages.forEach(function(commandPair){
+                commandPair.command.forEach(function(cmdText){
+                    if (cmdText == cmd){
+                        message.channel.send(commandPair.message);
+                    }
+                });
+            });
+        }
         isReady = true;
      } else if (isReady && message.content.substring(0,1) == "?"){
         isReady = false;
@@ -48,15 +64,28 @@ bot.on('message', message => {
         switch(cmd) {
             case 'commands':
                 //todo
+                var commandListString = "The Audio Commands are:\n";
+                var commandPrepend = "?";
+                audioFiles.forEach(function (audioClip){
+                    audioClip.SoundName.forEach(function (clipCommand){
+                        commandListString += commandPrepend + clipCommand + commandPostpend;
+                    });
+                    commandListString = commandListString.substring(0, commandListString.length - 2);
+                    commandListString += " **|** "
+                });
+                commandListString = commandListString.substring(0, commandListString.length - 7);
+                message.channel.send(commandListString);
                 isReady = true;
             break;
         }
 
         audioFiles.forEach(function(audioClip){
-            if(cmd == audioClip.SoundName){
-                playAudio(audioClip.SoundUrl, message);
-                isReady = true;
-            }
+            audioClip.SoundName.forEach(function (clipCommand){
+                if(cmd == clipCommand){
+                    playAudio(audioClip.SoundUrl, message);
+                    isReady = true;
+                }
+            })
         })
      }
 });
@@ -65,6 +94,7 @@ bot.login(auth.token);
 
 function playAudio(audioClipUrl, message){
     var voiceChannel = message.member.voiceChannel
+    if (voiceChannel !== undefined){
     voiceChannel.join()
         .then(connection => {
             const dispatcher = connection.playArbitraryInput(audioClipUrl);
@@ -74,4 +104,7 @@ function playAudio(audioClipUrl, message){
                 voiceChannel.leave();
             });
         }).catch(console.log);
+    } else {
+        message.channel.send("Please join a voice channel to use audio commands. \nAudio commands start with a '?'")
+    }
 }
